e648cf60e90cfecb10274cef15817c9c9fb330e3
dacaplan <dacaplan@scinet01.(none)>
2010-10-21 16:21:17 -0400

    wrapper seems to work

--- scripts/pymbar-wrapper.py
+++ scripts/pymbar-wrapper.py
@@ -8,11 +8,11 @@ import os
 import sys
 import optparse
 from math import pi
-import tmpfile
 import random
+import numpy
 
-import pymbar # multistate Bennett acceptance ratio
-import timeseries # timeseries analysis
+from pymbar import timeseries # timeseries analysis
+from pymbar import pymbar
 
 # Constants.
 kB = 1.381e-23 * 6.022e23 / 1000.0 # Boltzmann constant in kJ/mol/K
@@ -44,8 +44,8 @@ def main():
     parser.add_option("-o", "--outfile", dest="output_file", default='mbar_pmf.out', help="Output file for PMF [default: %default]")
     parser.add_option("-t", "--temperature", dest="temperature", default=300., type="float", help="Initial temperature in K [default: %default K]")
     
-    parser.add_option("--min", dest="min_coord", default=0, type="int", help="Min coordinate for 1D PMF [default: %default]")
-    parser.add_option("--max", dest="max_coord", default=360, type="int", help="Max coordinate for 1D PMF [default: %default]")
+    #parser.add_option("--min", dest="min_coord", default=0, type="int", help="Min coordinate for 1D PMF [default: %default]")
+    #parser.add_option("--max", dest="max_coord", default=360, type="int", help="Max coordinate for 1D PMF [default: %default]")
     parser.add_option("-b", "--bins", dest="bins", default=50, type="int", help="Number of bins for 1D PMF [default: %default]")
     
     (options, args) = parser.parse_args()
@@ -71,13 +71,13 @@ def main():
     for line in metadata_lines:
         if line.startswith('#'):
             continue
-        clean_split = filter(None, line.expandtabs().split(' '))
+        clean_split = filter(None, line.strip().expandtabs().split(' '))
         if not os.path.exists(clean_split[0]):
             print "Data file %s doesn't exist, skipping this replica" % clean_split[0]
             continue
         else:
-            snapshots = file_len(clean_split[0])
-            if snapshots > N_max:
+            nsnapshots = file_len(clean_split[0])
+            if nsnapshots > N_max:
                 N_max = nsnapshots
             
             # /path/to/timeseries/file  loc_win_min spring  [correl time] [temperature]
@@ -85,8 +85,8 @@ def main():
             # TODO: spring constant units?
             #   K_k[k] = float(tokens[1]) * (numpy.pi/180)**2 # spring constant (read in kJ/mol/rad**2, converted to kJ/mol/deg**2)    
             
-            if len(clean_split) == 3:
-                # TODO: temperature the 3rd or 4th value???
+            if len(clean_split) >= 4:
+                # TODO: temperature the 4rd or 5th value???
                 # temperature might be the 4th value...
                 current_meta['t'] = float(clean_split[3])
                 different_temperatures = True
@@ -94,10 +94,10 @@ def main():
             
     print "Max number of snapshots %d" % N_max
     
-    print ''
-    for m in metadata:
-        print m
-    print ''
+    #print ''
+    #for m in metadata:
+    #    print m
+    #print ''
     
     # now allocate the memory for the arrays
     K = len(metadata)
@@ -122,38 +122,30 @@ def main():
     u_kln = numpy.zeros([K,K,N_max], numpy.float64) # u_kln[k,l,n] is the reduced potential energy of snapshot n from umbrella simulation k evaluated at umbrella l
     
     g_k = numpy.zeros([K],numpy.float32) # statistical inefficiency?
+
+    data_min = None
+    data_max = None
     
     # Now loop through each datafile and extract the data
-            
-    # f = open(clean_split[0], 'r')
-    # data_lines = f.readlines()
-    # f.close()
-    # clean_split = filter(None, dline.expandtabs().split(' '))
-    #  if len(clean_split) < 2:
-    #      print "Skipping short line: %s" % dline
-    #      continue
-    # FORMAT: f.write('0.0    %f\n' % deg)
-    
     for i, w in enumerate(metadata):
         print "Reading %s..." % w['path']
         f = open(w['path'], 'r')
         lines = f.readlines()
         f.close()
         
-        clean_split = filter(None, dline.expandtabs().split(' '))
+        clean_split_lines = [ filter(None, line.strip().expandtabs().split(' ')) for line in lines if not line.startswith('#') ]
 
         if different_temperatures:
             # if different temperatures are specified the metadata file, 
             # then we need the energies to compute the PMF, found in the third column
-            for l in clean_split:
-                if not l[0].startswith('#'):
-                    data[i] = float(l[1]) # second column is the coordinate
-                    # third column will be the system's potential energy
-                    potential_energy = float(l[2])
-                    dchi = w['coord']-data[i]
-                    restraint_potential = 0.5*w['k']*(dchi**2)
-                    # TODO: given the coordinate and the restraining potential, calculate the umbrella restraint
-                    u_kn[i] = beta_k[i] * (potential_energy-restraint_potential) # reduced potential energy without umbrella restraint
+            for j,l in enumerate(clean_split_lines):
+                data[i,j] = float(l[1]) # second column is the coordinate
+                # third column will be the system's potential energy
+                potential_energy = float(l[2])
+                dchi = w['coord']-float(l[1])
+                restraint_potential = 0.5*w['k']*(dchi**2)
+                # TODO: given the coordinate and the restraining potential, calculate the umbrella restraint
+                u_kn[i,j] = beta_k[i] * (potential_energy-restraint_potential) # reduced potential energy without umbrella restraint
         
             # Compute correlation times for potential energy and timeseries.
             # If the temperatures differ, use energies to determine samples; otherwise, use the cosine of chi
@@ -162,16 +154,28 @@ def main():
         else:
             # no temperature column
             # FORMAT: f.write('0.0    %f\n' % deg)
-            data[i] = [ float(l[1]) for l in clean_split if not l[0].startswith('#') ]
-            g_k[i] = timeseries.statisticalInefficiency(numpy.cos(data[i,:]/(180.0/numpy.pi)),numpy.cos(data[i,:]/(180.0/numpy.pi)))
-            indices = timeseries.subsampleCorrelatedData(numpy.cos(data[i,:]/(180.0/numpy.pi)))
+            for j,l in enumerate(clean_split_lines):
+                data[i,j] = float(l[1])
+            dataset = numpy.cos(data[i,:w['n']])
+            g_k[i] = timeseries.statisticalInefficiency(dataset,dataset)
+            indices = timeseries.subsampleCorrelatedData(dataset)
+            #g_k[i] = timeseries.statisticalInefficiency(numpy.cos(data[i,:]/(180.0/numpy.pi)),numpy.cos(data[i,:]/(180.0/numpy.pi)))
+            #indices = timeseries.subsampleCorrelatedData(numpy.cos(data[i,:]/(180.0/numpy.pi)))
+
+        # get min and max for data, used for binning ranges
+        max = numpy.max(data[i,indices])
+        min = numpy.min(data[i,indices])
+        if data_max is None or data_max < max:
+            data_max = max
+        if data_min is None or data_min > min:
+            data_min = min
 
         print "Correlation time for set %5d is %10.3f" % (i,g_k[i])
 
         # Subsample the data
-        metadata[k]['n'] = len(indices)
-        u_kn[i,0:metadata[i]['n']] = u_kn[i,indices]
-        data[i,0:metadata[i]['n']] = data[i,indices]
+        w['n'] = len(indices)
+        u_kn[i,0:w['n']] = u_kn[i,indices]
+        data[i,0:w['n']] = data[i,indices]
 
     print "Finished reading data files"
     
@@ -180,28 +184,41 @@ def main():
 
     # Construct torsion bins
     print "Binning data..."
+    print "Min coord: %f" % data_min
+    print "Max coord: %f" % data_max
     
-    delta = (options.max_coord - options.min_coord) / float(options.bins)
+    delta = (data_max - data_min) / float(options.bins)
+    print "Delta for binning %f" % delta
     # compute bin centers
     bin_center_i = numpy.zeros([options.bins], numpy.float64)
     for i in range(options.bins):
-        bin_center_i[i] = options.min_coord + delta/2 + delta * i
+        bin_center_i[i] = data_min + delta/2 + delta * i
+    #print "Bin centers:"
+    #print bin_center_i
     
     # Bin data
     bin_kn = numpy.zeros([K,N_max], numpy.int32)
+    # for each window
     for k in range(K):
+        # for 0 to the number of snapshots in the window k
         for n in range(metadata[k]['n']):            
             # Compute bin assignment.
-            # replica index, snapshot index
-            bin_kn[k,n] = int((data[k,n] - options.min_coord) / delta)
+            bin_kn[k,n] = int((data[k,n] - data_min) / delta)
             for l in range(K):
                 # Compute minimum-image torsion deviation from umbrella center l
                 dchi = data[k,n] - metadata[l]['coord']
                 # Compute energy of snapshot n from simulation k in umbrella potential l
                 u_kln[k,l,n] = u_kn[k,n] + beta_k[k]*0.5*metadata[l]['k']*(dchi**2)
+    
+    for i in range(options.bins):
+        print "bin %d" % i
+        print numpy.sum(bin_kn==i)
 
     # Initialize MBAR.
     print "Running MBAR..."
+    N_k = numpy.array([ w['n'] for w in metadata ], numpy.int32)
+    print N_k
+    print u_kln
     mbar = pymbar.MBAR(u_kln, N_k, verbose=True, method='self-consistent-iteration', initialize='BAR')
     #mbar = pymbar.MBAR(u_kln, N_k, verbose = True, method = 'Newton-Raphson')
 
