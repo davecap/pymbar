3e5767aa97a297b6411d3f7841c7b2149a877028
dacaplan <dacaplan@scinet01.(none)>
2010-10-22 14:11:36 -0400

    fix a bug

--- scripts/pymbar-wrapper.py
+++ scripts/pymbar-wrapper.py
@@ -36,6 +36,7 @@ def main():
     parser.add_option("-o", "--outfile", dest="output_file", default='mbar_pmf.out', help="Output file for PMF [default: %default]")
     parser.add_option("-t", "--temperature", dest="temperature", default=300., type="float", help="Initial temperature in K [default: %default K]")
     parser.add_option("-b", "--bins", dest="bins", default=50, type="int", help="Number of bins for 1D PMF [default: %default]")
+    parser.add_option("-v", "--verbose", dest="verbose", default=False, action="store_true", help="Verbose output from PyMBAR [default: %default]")
     
     (options, args) = parser.parse_args()
     
@@ -90,7 +91,7 @@ def main():
     data = numpy.zeros([K,N_max], numpy.float64) # the snapshot data
     u_kn = numpy.zeros([K,N_max], numpy.float64) # u_kn[k,n] is the reduced potential energy without umbrella restraints of snapshot n of umbrella simulation k
     u_kln = numpy.zeros([K,K,N_max], numpy.float64) # u_kln[k,l,n] is the reduced potential energy of snapshot n from umbrella simulation k evaluated at umbrella l
-    g_k = numpy.zeros([K],numpy.float32) # statistical inefficiency
+    g_k = numpy.zeros([K],numpy.float32) # correlation time
 
     data_min = [] # will set the min and max data values later
     data_max = []
@@ -160,7 +161,7 @@ def main():
         bin_center_i[i] = data_min + delta/2 + delta * i
     
     # Bin data
-    bin_kn = numpy.zeros([K,N_max], numpy.int32)
+    bin_kn = numpy.zeros([K,N_max], numpy.int32)-1
     # for each window
     for k in range(K):
         # for 0 to the number of snapshots in the window k
@@ -175,15 +176,15 @@ def main():
     
     for i in range(options.bins):
         if numpy.sum(bin_kn==i) == 0:
-            for i in range(options.bins):
-                print "Bin: %d" % i
-                print numpy.sum(bin_kn==i)
+            for j in range(options.bins):
+                print "Bin: %d" % j
+                print numpy.sum(bin_kn==j)
             raise Exception("At least one bin has no samples. Adjust bin sizes or eliminate empty bins to ensure at least one sample per bin.")        
 
     # Initialize MBAR.
     print "Running MBAR..."
     N_k = numpy.array([ w['n'] for w in metadata ], numpy.int32)
-    mbar = pymbar.MBAR(u_kln, N_k, verbose=True, method='self-consistent-iteration', initialize='BAR')
+    mbar = pymbar.MBAR(u_kln, N_k, verbose=options.verbose, method='self-consistent-iteration', initialize='BAR')
     #mbar = pymbar.MBAR(u_kln, N_k, verbose = True, method = 'Newton-Raphson')
 
     # Compute PMF in unbiased potential (in units of kT).
@@ -194,7 +195,7 @@ def main():
     f = open(options.output_file, 'w')
     print "PMF (in units of kT)"
     print "%8s %8s %8s" % ('bin', 'f', 'df')
-    f.write("#Coor   Free    +/-\n" % ('bin', 'f', 'df'))
+    f.write("#Coor   Free    +/-\n")
     for i in range(options.bins):
         print "%8.1f %8.3f %8.3f" % (bin_center_i[i], f_i[i], df_i[i])
         f.write("%8.1f %8.3f %8.3f\n" % (bin_center_i[i], f_i[i], df_i[i]))
